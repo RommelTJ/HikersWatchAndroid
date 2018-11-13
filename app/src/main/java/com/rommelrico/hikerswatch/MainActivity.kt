30c460dff8d1bc154b28b7f3503d48ba2951b945
Rommel Rico <me@rommelrico.com>
2018-11-13 08:42:56 -0800

    startListening function

--- app/src/main/java/com/rommelrico/hikerswatch/MainActivity.kt
+++ app/src/main/java/com/rommelrico/hikerswatch/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.rommelrico.hikerswatch
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
 
@@ -77,5 +80,12 @@ class MainActivity : AppCompatActivity() {
 
         }
 
+    } // end onCreate
+
+    fun startListening() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager = this.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        }
     }
+
 }
