29ed366c3babdd81cb331a07ccda36500c30c73e
Rommel Rico <me@rommelrico.com>
2018-11-13 08:38:46 -0800

    LocationManager and LocationListener properties

--- app/src/main/java/com/rommelrico/hikerswatch/MainActivity.kt
+++ app/src/main/java/com/rommelrico/hikerswatch/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.rommelrico.hikerswatch
 
+import android.location.LocationListener
+import android.location.LocationManager
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+
+    var locationManager: LocationManager? = null
+    var locationListener: LocationListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
